69336f7199c6597ad2ab66324551a8a33aa20fe9
Gurkenglas <gurkenglas@hotmail.de>
2016-04-18 22:03:47 +0200

    Update Snake.hs

--- Snake.hs
+++ Snake.hs
@@ -6,6 +6,5 @@ data Snake = Snake { getDirection :: Direction
                    , getCoords :: [Coords] }
 
 instance Show Snake where
-    show (Snake direction []) = "Snake " ++ show direction ++ " []"
     show (Snake direction coords) = "Snake " ++ show direction ++ " [" ++ unwords (map showSnakeCoord coords) ++ "]" where
         showSnakeCoord (Coords x y) = "(" ++ show x ++ " " ++ show y ++ ")"
